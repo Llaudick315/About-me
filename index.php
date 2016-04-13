7d56ddecc07750a1b4924dab15d9bf1939325ab3
Lucas Lauick <llaudick@cnm.edu>
2016-04-13 13:26:20 -0600

    added picture to About-me

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 		</head>
 		<body>
 			<h1>My Name Is Lucas Laudick</h1>
-			<p>I was actually born in Oklahoma city, but moved to Albuquerque a couple months later where I went to school through highschool. I then went to Durango, CO on a golf scholarship to Fort Lewis College where I met my wife. We just got married in Maui, HI on February 5, 2016! We have two dogs one of which is only 12 weeks old and can be a pain in our butt sometimes but we still love her, well....sorta. Some of the things I love to do would have to include golfing, snowboarding, hiking, and just hanging out with family and friends. </p>
+			<p>I was actually born in Oklahoma city, but moved to Albuquerque a couple months later where I went to school through highschool. I then went to Durango, CO on a golf scholarship to Fort Lewis College where I met my wife. We just got married in Maui, HI on February 5, 2016!</p>
+			<img src="images/makena-beach.jpg">
+			<p> We have two dogs one of which is only 12 weeks old and can be a pain in our butt sometimes, but we still love her, well....sorta. Some of the things I love to do would have to include golfing, snowboarding, hiking, and just hanging out with family and friends. </p>
 		</body>
 </html>
